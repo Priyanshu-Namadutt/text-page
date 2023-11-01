233911d0b0fa63c74614d162acd3b5f9349095c2
Priyanshu Namadutt <priyanshunamadutt999@gmail.com>
2023-11-01 23:07:54 +0530

    add files

---
+++ script.js
@@ -0,0 +1,9 @@
+let logref = document.getElementById("log");
+
+let signref = document.getElementById("sign");
+
+logref.addEventListener('click' , function ()  {
+
+    window.open('https://www.youtube.com/')
+});
+
